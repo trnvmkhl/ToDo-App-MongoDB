4006214708fa00ba646f57d23ea66c80820ec7d0
trnvmkhl <trnvmkhl@gmail.com>
2021-07-26 05:19:57 +0300

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -165,6 +165,16 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
   console.log("Server started on port 3000");
 });
+
+
+// app.listen(process.env.PORT || 3000, function() {
+//   console.log("Server started on port 3000");
+// });
